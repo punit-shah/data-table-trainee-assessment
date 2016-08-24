7c8e405d827252cbb1709cdd870e2af020f8b3c6
Punit Shah <punit.shah@bbc.co.uk>
2016-08-24 10:16:31 +0100

    added line chart with Chart.js

---
+++ data-chart.js
@@ -0,0 +1,73 @@
+var channels = {
+    bbcone: 'BBC One',
+    bbctwo: 'BBC Two',
+    bbcthree: 'BBC Three',
+    bbcfour: 'BBC Four',
+    bbcnews24: 'BBC News 24',
+    cbbc: 'CBBC',
+    cbeebies: 'Cbeebies',
+};
+
+function getChartData(data) {
+    return {
+        labels: getChartLabels(data),
+        datasets: getChartDatasets(data),
+    };
+}
+
+function getChartLabels(data) {
+    var labels = [];
+
+    for (var date in data) {
+        labels.push(convertDate(date));
+    }
+
+    return labels;
+}
+
+function getChartDatasets(data) {
+    var datasets = [];
+
+    var colors = {
+        bbcone: '#ea2923',
+        bbctwo: '#005761',
+        bbcthree: '#d41c6f',
+        bbcfour: '#1e1e1e',
+        bbcnews24: '#900',
+        cbbc: '#9fe600',
+        cbeebies: '#29c5fe',
+    };
+
+    for (var channel in channels) {
+        var dataset = {
+            fill: false,
+            lineTension: 0.1,
+            pointRadius: 1,
+            pointHoverRadius: 5,
+            pointHitRadius: 20,
+            spanGaps: false,
+        };
+
+        dataset.label = channels[channel];
+        dataset.data = [];
+        dataset.borderColor = colors[channel];
+        dataset.backgroundColor = colors[channel];
+
+        for (var date in data) {
+            dataset.data.push(data[date][channel]);
+        }
+
+        datasets.push(dataset);
+    }
+
+    return datasets;
+}
+
+function makeChart(data) {
+    var canvas = $('#data-chart');
+    var data = getChartData(data);
+    var chart = new Chart(canvas, {
+        type: 'line',
+        data: data
+    });
+}
