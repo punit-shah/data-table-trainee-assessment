1de8110e97728f64398c95cf425e497b4b60f0ae
Punit Shah <punit.shah@bbc.co.uk>
2016-08-24 11:01:37 +0100

    implemented tabs

---
+++ tabs.js
@@ -0,0 +1,20 @@
+function initTabs() {
+    $('.tab').click(function () {
+        $('.tab').removeClass('selected');
+        $(this).addClass('selected');
+        showHideTabContent($(this).attr('data-shows'));
+    });
+
+    showHideTabContent();
+    $('.tab')
+        .first()
+        .click();
+}
+
+// hides all tab content before showing the element passed in
+function showHideTabContent(selector) {
+    $('[data-tab-content]').hide();
+    if (selector) {
+        $(selector).show();
+    }
+}
