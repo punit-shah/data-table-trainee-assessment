4b20f5d5bb13902b55b074610508e29449c751c9
Punit Shah <punit.shah@bbc.co.uk>
2016-08-24 11:23:32 +0100

    using 2 spaces for indentation

--- data-chart.js
+++ data-chart.js
@@ -1,74 +1,71 @@
 function makeChart(canvas, data, channels) {
-    var canvas = $(canvas);
-    var data = getChartData(data, channels);
-    var chart = new Chart(canvas, {
-        type: 'line',
-        data: data,
-        options: {
-            legend: {
-                position: 'bottom',
-                labels: {
-                    boxWidth: 10
-                },
-            },
-            tooltips: {
-                'mode': 'x-axis',
-            },
+  var canvas = $(canvas);
+  var chart = new Chart(canvas, {
+    type: 'line',
+    data: getChartData(data, channels),
+    options: {
+      legend: {
+        position: 'bottom',
+        labels: {
+          boxWidth: 10
         },
-    });
+      },
+      tooltips: {
+        'mode': 'x-axis',
+      },
+    },
+  });
 }
 
 function getChartData(data) {
-    return {
-        labels: getChartLabels(data),
-        datasets: getChartDatasets(data, channels),
-    };
+  return {
+    labels: getChartLabels(data),
+    datasets: getChartDatasets(data, channels),
+  };
 }
 
 function getChartLabels(data) {
-    var labels = [];
+  var labels = [];
 
-    for (var date in data) {
-        labels.push(convertDate(date));
-    }
+  for (var date in data) {
+    labels.push(convertDate(date));
+  }
 
-    return labels;
+  return labels;
 }
 
 function getChartDatasets(data, channels) {
-    var datasets = [];
+  var datasets = [];
 
-    var colors = {
-        bbcone: '#ea2923',
-        bbctwo: '#005761',
-        bbcthree: '#d41c6f',
-        bbcfour: '#1e1e1e',
-        bbcnews24: '#900',
-        cbbc: '#9fe600',
-        cbeebies: '#29c5fe',
+  var colors = {
+    bbcone: '#ea2923',
+    bbctwo: '#005761',
+    bbcthree: '#d41c6f',
+    bbcfour: '#1e1e1e',
+    bbcnews24: '#900',
+    cbbc: '#9fe600',
+    cbeebies: '#29c5fe',
+  };
+
+  for (var channel in channels) {
+    var dataset = {
+      fill: false,
+      lineTension: 0.1,
+      pointRadius: 1,
+      pointHoverRadius: 5,
+      pointHitRadius: 50,
+      borderColor: colors[channel],
+      backgroundColor: colors[channel],
+      label: channels[channel],
+      data: [],
     };
 
-    for (var channel in channels) {
-        var dataset = {
-            fill: false,
-            lineTension: 0.1,
-            pointRadius: 1,
-            pointHoverRadius: 5,
-            pointHitRadius: 50,
-            spanGaps: false,
-        };
-
-        dataset.label = channels[channel];
-        dataset.data = [];
-        dataset.borderColor = colors[channel];
-        dataset.backgroundColor = colors[channel];
-
-        for (var date in data) {
-            dataset.data.push(data[date][channel]);
-        }
-
-        datasets.push(dataset);
+    for (var date in data) {
+      dataset.data.push(data[date][channel]);
     }
 
-    return datasets;
+    datasets.push(dataset);
+  }
+
+  return datasets;
 }
